95e2d7499dd8c26f44329d0957944c702711236a
farruholimov <farruhbekolimov3904@gmail.com>
2022-06-27 17:37:10 +0500

    fix

--- src/db/models/notifications/notifications.js
+++ src/db/models/notifications/notifications.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
             }
         },
         notifier_id: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.UUID,
             allowNull: false,
             references: {
               model: "users",
