47c584f0d366f271ae44d6a52a222d6ffc89c97b
farruholimov <farruhbekolimov3904@gmail.com>
2022-07-14 15:44:02 +0500

    update

--- src/routes/ads.js
+++ src/routes/ads.js
@@ -1,10 +1,11 @@
-const { GetAll, GetOne, Create, Update, Delete} = require("../controllers/ads")
+const { GetAll, GetOne, Create, Update, Delete, GetAmountRange} = require("../controllers/ads")
 const protect = require("../middlewares/auth/protect")
 
 const AdsRouter = require("express").Router()
 
 AdsRouter.get("/", GetAll)
 AdsRouter.get("/:id", GetOne)
+AdsRouter.get("/range", GetAmountRange)
 AdsRouter.post("/", Create)
 AdsRouter.put("/:id", Update)
 AdsRouter.delete("/:id", Delete)
